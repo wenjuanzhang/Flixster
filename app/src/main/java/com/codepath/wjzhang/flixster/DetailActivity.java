8d9e7f5cd7bcec2eeaad2273abbced8afff45e3b
wenjuanzhang <zhang.6045@osu.edu>
2020-10-02 17:28:54 -0400

    finish required and stretch stories of week 2 assignment

---
+++ app/src/main/java/com/codepath/wjzhang/flixster/DetailActivity.java
@@ -0,0 +1,98 @@
+package com.codepath.wjzhang.flixster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.codepath.wjzhang.flixster.models.Movie;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.parceler.Parcels;
+
+import okhttp3.Headers;
+
+public class DetailActivity extends YouTubeBaseActivity {
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyB25Xwzict_4m19V6qX88GQxT2f7bXSVfI";
+    public static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    TextView tvTitle;
+    TextView tvOverview;
+    RatingBar ratingBar;
+    YouTubePlayerView youTubePlayerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        tvTitle = findViewById(R.id.tvTitle);
+        tvOverview = findViewById(R.id.tvOverview);
+        ratingBar = findViewById(R.id.ratingBar);
+        youTubePlayerView = findViewById(R.id.player);
+
+        String title = getIntent().getStringExtra("title");
+        final Movie movie= Parcels.unwrap(getIntent().getParcelableExtra("movie"));
+        // tvTitle.setText(title);
+        tvTitle.setText(movie.getTitle());
+        tvOverview.setText(movie.getOverview());
+        ratingBar.setRating((float)movie.getRating());
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(String.format(VIDEOS_URL, movie.getMovieId()), new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                try {
+                    JSONArray results = json.jsonObject.getJSONArray("results");
+                    if (results.length() == 0){
+                        return;
+                    }
+                    String youtubeKey = results.getJSONObject(0).getString("key");
+                    Log.d("DetailActivity", youtubeKey);
+                    initializeYoutube(youtubeKey, movie);
+                } catch (JSONException e) {
+                    Log.e("DetailActivity","Failed to parse JSON",e);
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+
+            }
+        });
+
+
+    }
+
+    private void initializeYoutube(final String youtubeKey, final Movie movie) {
+        youTubePlayerView.initialize(YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                Log.d("DetailActivity","onInitializationSuccess");
+
+                if (movie.getRating() >= 6) {// if rating is greater than 6 out 10, play trailer automatically
+                    youTubePlayer.loadVideo(youtubeKey);
+                    youTubePlayer.play();
+                }
+                else{// else, give a preview of trailer for movies with rating less than 6 out of 10
+                    youTubePlayer.cueVideo(youtubeKey);
+                }
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Log.d("DetailActivity","onInitializationFailure");
+            }
+        });
+    }
+}
--- app/src/main/java/com/codepath/wjzhang/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/codepath/wjzhang/flixster/adapters/MovieAdapter.java
@@ -1,22 +1,48 @@
 package com.codepath.wjzhang.flixster.adapters;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.Drawable;
+import android.os.Parcel;
 import android.util.Log;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.util.Pair;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.MultiTransformation;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.bitmap.FitCenter;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+import com.codepath.wjzhang.flixster.DetailActivity;
+import com.codepath.wjzhang.flixster.MainActivity;
 import com.codepath.wjzhang.flixster.R;
 import com.codepath.wjzhang.flixster.models.Movie;
 
+import org.parceler.Parcels;
+
 import java.util.List;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
 
@@ -55,20 +81,28 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
+        TextView tvRating;
         ImageView ivPoster;
+        ImageView ivPlayBtn;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
+            tvRating = itemView.findViewById(R.id.tvRating);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            ivPlayBtn = itemView.findViewById(R.id.ivPlayBtn);
+            container = itemView.findViewById(R.id.container);
         }
 
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
+            tvRating.setText(String.format("Rating: %.1f/10", (float) movie.getRating()));
+
             String imageUrl;
             // if phone is in landscape
             if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
@@ -79,7 +113,53 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
                 imageUrl = movie.getPosterPath();
             }
 
-            Glide.with(context).load(imageUrl).into(ivPoster);
+//            Glide.with(context).load(imageUrl).into(ivPoster);
+            final ProgressBar progressBar =itemView.findViewById(R.id.loadingBar);
+            int radius = 30; // corner radius, higher value = more rounded
+            int margin = 8; // crop margin, set to 0 for corners with no crop
+            Glide.with(context)
+                    .load(imageUrl)
+                    //.placeholder(Drawable.createFromPath("ic_pineapple_icon"))
+                    .listener(new RequestListener<Drawable>() {
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                            progressBar.setVisibility(View.GONE);
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            progressBar.setVisibility(View.GONE);
+                            if (movie.getRating() < 6){
+                                ivPlayBtn.setVisibility(View.GONE);
+                            }
+                            else{
+                                ivPoster.setColorFilter(new PorterDuffColorFilter(Color.argb(50, 0, 0, 0), PorterDuff.Mode.SRC_ATOP));
+                            }
+                            return false;
+                        }
+                    })
+                    .fitCenter()
+                    .transform(new MultiTransformation(new FitCenter(), new RoundedCornersTransformation(radius, margin)))
+                    //.transform(new RoundedCornersTransformation(radius, margin))
+                    .into(ivPoster);
+            // 1. Register click listener on the whole row
+
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // 2. Navigate to a new activity on tap
+                    Intent i = new Intent(context, DetailActivity.class);
+                    //i.putExtra("title",movie.getTitle());
+                    Pair<View, String> p1 = Pair.create((View)tvTitle, "title");
+                    Pair<View, String> p2 = Pair.create((View)tvOverview, "overview");
+                    ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation((Activity) context, p1, p2);
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i, options.toBundle());
+//                    Toast.makeText(context, movie.getTitle(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
         }
     }
 }
